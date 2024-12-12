ce758f283a379e7b0d5a0c651301ba34eaaccad8
l02 <l8556@outlook.com>
2024-12-12 19:13:17 +0300

    refactoring

--- tests/desktop_tests/tools/paths/paths.py
+++ tests/desktop_tests/tools/paths/paths.py
@@ -8,4 +8,4 @@ class Paths:
     def __init__(self, os_type: str, remote_user_name: str = None):
         self.local = LocalPaths()
         if remote_user_name:
-            self.remote = RemotePaths(user_name=remote_user_name, windows=('windows' in os_type.lower()))
+            self.remote = RemotePaths(user_name=remote_user_name, os_type=os_type)
