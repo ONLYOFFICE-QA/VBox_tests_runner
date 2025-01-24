598e3e4cde0efb7439338acbd109e65e64c77f09
l02 <l8556@outlook.com>
2025-01-24 18:51:45 +0300

    refactoring

--- tests/desktop_tests/tools/test_tools/vbox_utils/schtasks_command.py
+++ tests/desktop_tests/tools/test_tools/vbox_utils/schtasks_command.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-
 class SchtasksCommand:
 
     def __init__(self, task_name: str):
         self.task_name = task_name
 
     def status(self) -> str:
-        return f'schtasks /query /tn "{self.task_name}" /v /fo LIST'
+        return self._build_command("query", "/v /fo LIST")
 
     def create(self, command: str) -> str:
-        return f'schtasks /create /tn "{self.task_name}" /tr "{command}" /sc onstart /rl highest'
+        return self._build_command("create", f'/tr "{command}" /sc onstart /rl highest')
 
     def run(self) -> str:
-        return f'schtasks /run /tn "{self.task_name}"'
+        return self._build_command("run")
+
+    def _build_command(self, action: str, additional: str = "") -> str:
+        return f'schtasks /{action} /tn "{self.task_name}" {additional}'.strip()
