c0d53a6fab300b2294e121eef2d9182a24af6241
Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>
2025-04-10 18:11:32 +0300

    .

--- tests/desktop_tests/__init__.py
+++ tests/desktop_tests/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .tools import DesktopReport, DesktopTestData
 from .desktop_tests import DesktopTest
-from . import multiprocessing
+from .. import multiprocessing
 
 __all__ = [DesktopReport, DesktopTest, multiprocessing]
