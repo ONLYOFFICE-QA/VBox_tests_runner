403f8df226074e35a4c84ea864cc676e75903102
Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>
2025-05-05 12:16:19 +0300

    refactoring

--- frameworks/report_portal/portal_manager.py
+++ frameworks/report_portal/portal_manager.py
@@ -12,13 +12,12 @@ class PortalManager:
         self.rp = ReportPortal(project_name=project_name)
         self.__suites = None
         self.__suite_names = None
-        self.__tests_items = None
         self.__steps_items = None
 
     @property
     def suites(self) -> list:
         if self.__suites is None:
-            self.__suites = self.rp.request.suite.get_suites(launch_id=self.rp.launch.id)
+            self.__suites = self.rp.get_launch_suite().get_items_by_type()
         return self.__suites
 
     @property
@@ -30,14 +29,8 @@ class PortalManager:
     @property
     def steps_items(self) -> list:
         if self.__steps_items is None:
-            self.__steps_items = self.rp.request.step.get_steps(launch_id=self.rp.launch.id)
+            self.__steps_items = self.rp.get_launch_step().get_items_by_type()
         return self.__steps_items
-
-    @property
-    def tests_items(self) -> list:
-        if self.__tests_items is None:
-            self.__tests_items = self.rp.request.test.get_tests(launch_id=self.rp.launch.id)
-        return self.__tests_items
 
     def __enter__(self):
         self.start_launch()
@@ -51,7 +44,7 @@ class PortalManager:
 
     def set_test_result(self, test_name: str, return_code: int, log_message: str = None, suite_uuid: str = None):
         step = self.rp.get_launch_step()
-        suite_id = step.request.get_id(uuid=suite_uuid, cache=True)
+        suite_id = self.rp.launch.rp_client.get_id(item_type='suite', uuid=suite_uuid, cache=True)
         exist_step = self.get_exist_item(self.steps_items, test_name, suite_id)
 
         step_uuid = step.start(
@@ -60,15 +53,16 @@ class PortalManager:
             retry=True if exist_step else False,
             uuid=exist_step["uuid"] if exist_step else None
         )
+
         step.send_log(
-            message=f"Test started at {time.strftime('%Y-%m-%d %H:%M:%S')}",
+            message=f"Test {test_name} started at {time.strftime('%Y-%m-%d %H:%M:%S')}",
             level="INFO",
             item_uuid=step_uuid
         )
 
         if str(log_message).lower() != 'nan':
             step.send_log(
-                message=log_message,
+                message=f"Test {test_name} log\n{log_message}",
                 level="ERROR" if return_code != 0 else "WARN",
                 item_uuid=step_uuid,
                 print_output=True
@@ -76,19 +70,19 @@ class PortalManager:
 
         step.finish(return_code=return_code)
 
-    def create_suite(self, suite_name: str, parent_suite_id: Optional[str] = None):
-        cache_key = f"{suite_name}_{parent_suite_id}"
+    def create_suite(self, suite_name: str, parent_suite_uuid: Optional[str] = None):
+        cache_key = f"{suite_name}_{parent_suite_uuid}"
 
         if cache_key not in self._suite_cache:
             suite = self.rp.get_launch_suite()
-            parent_id = suite.request.get_id(parent_suite_id) if parent_suite_id else None
+            parent_id = suite.get_id(parent_suite_uuid) if parent_suite_uuid else None
             exists_suite = self.get_exist_item(self.suites, suite_name, parent_id)
 
             if exists_suite:
                 self._suite_cache[cache_key] = exists_suite["uuid"]
             else:
                 self._suite_cache[cache_key] = suite.create(
-                    name=suite_name, parent_item_id=parent_suite_id
+                    name=suite_name, parent_item_id=parent_suite_uuid
                 )
 
         return self._suite_cache[cache_key]
