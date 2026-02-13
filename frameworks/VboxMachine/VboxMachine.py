ad14a2b415f4c2c1faa80209db3dd62194247db4
Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>
2026-02-13 13:19:09 +0300

    Update USB controller settings in VboxMachine to support ARM architecture
    - Modified the `VBoxMachine` class to enable the `xhci_controller` and `controller` methods based on the host architecture, specifically for ARM. This change enhances compatibility and performance for ARM-based systems.
    
    Co-authored-by: Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>

--- frameworks/VboxMachine/VboxMachine.py
+++ frameworks/VboxMachine/VboxMachine.py
@@ -144,8 +144,8 @@ class VboxMachine:
         memory allocation, audio settings, and speculative execution control.
         """
         self.vm.usb.ehci_controller(False)
-        self.vm.usb.xhci_controller(False)
-        self.vm.usb.controller(False)
+        self.vm.usb.xhci_controller(HostInfo().is_arm)
+        self.vm.usb.controller(HostInfo().is_arm)
         self.set_network_adapter()
         self.vm.set_cpus(self._get_cpu_num())
         self.vm.nested_virtualization(self.vm_config.nested_virtualization)
